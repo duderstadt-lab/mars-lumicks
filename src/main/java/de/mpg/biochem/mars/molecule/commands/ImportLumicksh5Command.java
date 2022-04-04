5221a72e743aaef24d3582a8974274d6b7e483dd
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-04-04 13:26:31 +0200

    working basic LUMICKS importer for simple two bead force extension data

--- src/main/java/de/mpg/biochem/mars/molecule/commands/ImportLumicksh5Command.java
+++ src/main/java/de/mpg/biochem/mars/molecule/commands/ImportLumicksh5Command.java
@@ -28,51 +28,32 @@
  */
 package de.mpg.biochem.mars.molecule.commands;
 
-import java.awt.AWTEvent;
-import java.awt.Choice;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Vector;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
-import javax.swing.ButtonGroup;
-import javax.swing.JRadioButton;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.decimal4j.util.DoubleRounder;
-import org.scijava.ItemVisibility;
+import org.scijava.ItemIO;
 import org.scijava.app.StatusService;
 import org.scijava.command.Command;
 import org.scijava.command.DynamicCommand;
 import org.scijava.log.LogService;
 import org.scijava.menu.MenuConstants;
-import org.scijava.module.MutableModuleItem;
 import org.scijava.plugin.Menu;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.table.DoubleColumn;
 import org.scijava.ui.UIService;
-import org.scijava.widget.ChoiceWidget;
 
-import de.mpg.biochem.mars.image.Peak;
-import de.mpg.biochem.mars.metadata.MarsMetadata;
-import de.mpg.biochem.mars.molecule.AbstractMoleculeArchive;
-import de.mpg.biochem.mars.molecule.Molecule;
-import de.mpg.biochem.mars.molecule.MoleculeArchive;
-import de.mpg.biochem.mars.molecule.MoleculeArchiveIndex;
-import de.mpg.biochem.mars.molecule.MoleculeArchiveProperties;
+import ch.systemsx.cisd.hdf5.HDF5Factory;
+import ch.systemsx.cisd.hdf5.IHDF5Reader;
+import de.mpg.biochem.mars.metadata.MarsOMEMetadata;
 import de.mpg.biochem.mars.molecule.MoleculeArchiveService;
 import de.mpg.biochem.mars.molecule.SingleMolecule;
+import de.mpg.biochem.mars.molecule.SingleMoleculeArchive;
 import de.mpg.biochem.mars.table.MarsTable;
 import de.mpg.biochem.mars.util.LogBuilder;
-import net.imagej.ops.Initializable;
-import org.scijava.table.DoubleColumn;
-
-import javax.swing.JLabel;
+import de.mpg.biochem.mars.util.MarsMath;
 
 @Plugin(type = Command.class, label = "LUMICKS h5", menu = {
 		@Menu(label = MenuConstants.PLUGINS_LABEL, weight = MenuConstants.PLUGINS_WEIGHT,
@@ -82,7 +63,7 @@ import javax.swing.JLabel;
 		@Menu(label = "Import", weight = 110,
 			mnemonic = 'i'),
 		@Menu(label = "LUMICKS h5", weight = 1, mnemonic = 'l')})
-public class ImportLumicksh5Command extends DynamicCommand implements Command, Initializable {
+public class ImportLumicksh5Command extends DynamicCommand implements Command {
 
 	@Parameter
 	private LogService logService;
@@ -95,26 +76,23 @@ public class ImportLumicksh5Command extends DynamicCommand implements Command, I
 
 	@Parameter
     private UIService uiService;
+	
+	@Parameter(label = "LUMICKS h5 file")
+	private File file;
+	
+	@Parameter(label = "Downsample")
+	private boolean downsample = false;
+	
+	@Parameter(label = "Downsample to Hz")
+	private int downsampleToHz = 10000;
+	
+	/**
+	 * OUTPUTS
+	 */
+	@Parameter(label = "Molecule Archive", type = ItemIO.OUTPUT)
+	private SingleMoleculeArchive archive;
 
-	//@Parameter(label = "X Column", choices = { "a", "b", "c" })
-	//private String xColumn;
-
-
-	@Override
-	public void initialize() {
-		//ArrayList<String> columns = new ArrayList<String>();
-		//columns.addAll(moleculeArchiveService.getArchives().get(0).properties()
-		//	.getColumnSet());
-		//columns.sort(String::compareToIgnoreCase);
-
-		//final MutableModuleItem<String> xColumnItems = getInfo().getMutableInput(
-		//	"xColumn", String.class);
-		//xColumnItems.setChoices(columns);
-
-		//final MutableModuleItem<String> yColumnItems = getInfo().getMutableInput(
-		//	"yColumn", String.class);
-		//yColumnItems.setChoices(columns);
-	}
+	private IHDF5Reader reader;
 
 	@Override
 	public void run() {
@@ -125,6 +103,8 @@ public class ImportLumicksh5Command extends DynamicCommand implements Command, I
 		LogBuilder builder = new LogBuilder();
 
 		String log = LogBuilder.buildTitleBlock("LUMICKS h5 Import");
+		
+		reader = HDF5Factory.openForReading(file.getAbsolutePath());
 
 		addInputParameterLog(builder);
 		log += builder.buildParameterList();
@@ -132,18 +112,56 @@ public class ImportLumicksh5Command extends DynamicCommand implements Command, I
 		//Output first part of log message...
 		logService.info(log);
 
-		final IHDF5Reader hdf5Reader = HDF5Factory.openForReading(testDirPath);
-
-		final HDF5DataSetInformation datasetInfo = reader.object().getDataSetInformation(pathName);
-
-		//Read stuff from the h5...
-		final HDF5DataSetInformation datasetInfo = reader.object().getDataSetInformation(pathName);
-			final long[] dimensions = datasetInfo.getDimensions();
-			int[] blockSize = overrideBlockSize ? null : datasetInfo.tryGetChunkSizes();
-
-			final HDF5DataTypeInformation attributeInfo = reader.object().getAttributeInformation(pathName, key);
-
-			///
+		archive = new SingleMoleculeArchive("Imported LUMICKS archive");
+		String metaUID = MarsMath.getUUID58().substring(0, 10);
+		MarsOMEMetadata meta = archive.createMetadata(metaUID);
+		
+		meta.setParameter("Bluelake version", reader.string().getAttr("/", "Bluelake version"));
+		meta.setParameter("Export time (ns)", reader.int64().getAttr("/", "File format version"));
+		meta.setParameter("File format version", reader.uint64().getAttr("/", "Export time (ns)"));
+		meta.setParameter("GUID", reader.string().getAttr("/", "GUID"));
+		
+		meta.addNote("Description: " + reader.string().getAttr("/", "Description"));
+		meta.addNote("Experiment: " + reader.string().getAttr("/", "Experiment"));
+		
+		//def members = reader.getGroupMembers("/Force HF/")
+		
+		archive.putMetadata(meta);
+		
+		double samplingRate = reader.uint64().getAttr("/Force HF/Force 1x/", "Sample rate (Hz)");
+		
+		double[] force1x = (downsample) ? 
+				downsample(reader.float64().readArray("/Force HF/Force 1x"), (int)Math.round(samplingRate/downsampleToHz)) 
+				: reader.float64().readArray("/Force HF/Force 1x");
+		double[] force2x = (downsample) ?
+				downsample(reader.float64().readArray("/Force HF/Force 2x"), (int)Math.round(samplingRate/downsampleToHz)) 
+				: reader.float64().readArray("/Force HF/Force 2x");
+		
+		if (downsample) samplingRate = downsampleToHz;
+				
+		MarsTable table = new MarsTable("Force table");
+		double[] time = new double[force1x.length];
+		for (int i = 0;i < time.length; i++) {
+			time[i] = i/samplingRate;
+		}
+		DoubleColumn tCol = new DoubleColumn("Time_(s)");
+		tCol.setArray(time);
+		tCol.setSize(time.length);
+		table.add(tCol);
+		
+		DoubleColumn force1xCol = new DoubleColumn("Force_1x");
+		force1xCol.setArray(force1x);
+		force1xCol.setSize(force1x.length);
+		table.add(force1xCol);
+		
+		DoubleColumn force2xCol = new DoubleColumn("Force_2x");
+		force2xCol.setArray(force2x);
+		force2xCol.setSize(force2x.length);
+		table.add(force2xCol);
+						
+		SingleMolecule molecule = archive.createMolecule(MarsMath.getUUID58(), table);
+		molecule.setMetadataUID(metaUID);
+		archive.put(molecule);
 
 		logService.info("Time: " + DoubleRounder.round((System.currentTimeMillis() - starttime)/60000, 2) + " minutes.");
 	    logService.info(LogBuilder.endBlock(true));
@@ -151,43 +169,29 @@ public class ImportLumicksh5Command extends DynamicCommand implements Command, I
 	}
 
 	private void addInputParameterLog(LogBuilder builder) {
-		builder.addParameter("xxx", xxx;
+		builder.addParameter("Downsample", downsample);
+		builder.addParameter("Downsample to Hz", downsampleToHz);
 	}
-
-	public String[] list(String pathName) throws IOException {
-
-		if (pathName.equals(""))
-			pathName = "/";
-
-		try {
-			final List<String> members = reader.object().getGroupMembers(pathName);
-			return members.toArray(new String[members.size()]);
-		} catch (final Exception e) {
-			throw new IOException(e);
+	
+	private static double[] downsample(double[] array, int factor) {
+		double[] downsampled = new double[(int)Math.floor(array.length/factor)];
+		
+		int binSize = (int)Math.floor(array.length/downsampled.length);
+		
+		double sum = 0;
+		int count = 0;
+		int index = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			count++;
+			if (count == binSize && index < downsampled.length) {
+				downsampled[index] = sum/count; 
+				count = 0;
+				sum = 0;
+				index++;
+			}
 		}
-	}
-
-	/**
-	 * Structured attributes that are stored as JSON will be classified as
-	 * String.class.
-	 */
-	@Override
-	public Map<String, Class<?>> listAttributes(final String pathName) throws IOException {
-
-		final String finalPathName = pathName.equals("") ? "/" : pathName;
-
-		final HashMap<String, Class<?>> attributes = new HashMap<>();
-		reader
-				.object()
-				.getAttributeNames(finalPathName)
-				.forEach(
-						attributeName -> attributes
-								.put(
-										attributeName,
-										reader
-												.object()
-												.getAttributeInformation(finalPathName, attributeName)
-												.tryGetJavaType()));
-		return attributes;
+		
+		return downsampled;
 	}
 }
